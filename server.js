1f5dc43b1a6799b059c6a2e69440f205ca2c40df
ayoubImpossible <azamani@csefrs.ma>
2025-04-26 12:13:03 +0000

    Fix package.json for deployment

--- server.js
+++ server.js
@@ -6,34 +6,39 @@ const categorieRoutes = require('./src/router/categorieRouter');
 const levelRoutes = require('./src/router/levelRouter');
 const typesRoutes = require('./src/router/typesRoutes');
 const atackRoutes = require('./src/router/atackMusculeRouter');
-const videoByRoutes = require('./src/router/videoByRouter');
-const plansRouter = require('./src/router/planRouter');
-const workoutsRouter = require('./src/router/workoutRoutes');
+const videoByRoutes=require('./src/router/videoByRouter');
+const plansRouter=require('./src/router/planRouter')
+const workoutsRouter=require('./src/router/workoutRoutes')
 const protectedRoutes = require('./src/router/protected');
 const savedcategoryRouter = require('./src/router/savedRouter');
-const profileRouter = require('./src/router/profileRouter');
-const searchRouter = require('./src/router/searchRouter');
+const profileRouter=require('./src/router/profileRouter')
+const searchRouter=require('./src/router/searchRouter')
 
-const helmet = require('helmet');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+
+const helmet =require('helmet') ;
+const bodyParser =require('body-parser') ;
+const cookieParser =require( 'cookie-parser') ;
+
 const cors = require('cors');
 const verifyToken = require('./src/Utils/VerifyToken');
+const app=express();
 
-// Import serverless-http
-const serverless = require('serverless-http'); 
-
-const app = express();
 
 app.use(express.json());
-dotenv.config();
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(helmet());
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.disable('x-powered-by');
 
+dotenv.config();
+
+app.use(express.urlencoded({extended:true}))
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+
+app.disable('x-powered-by');
 app.use('/api/exercises', exerciseRoutes);
 app.use('/api/users', userRoutes);    
 app.use('/api/categorie', categorieRoutes); 
@@ -43,16 +48,21 @@ app.use('/api/atack', atackRoutes);
 app.use('/api/Bytime', videoByRoutes); 
 app.use('/api/Plans', plansRouter); 
 app.use('/api/Workouts', workoutsRouter); 
-app.use('/api/protected', protectedRoutes);
-app.use('/api/protected', protectedRoutes);
-app.use('/api', savedcategoryRouter);
-app.use('/api/profile', profileRouter);
-app.use('/api', searchRouter);
+app.use('/api/protected',protectedRoutes);
+app.use('/api/protected',protectedRoutes);
+app.use('/api',savedcategoryRouter);
+app.use('/api/profile',profileRouter);
+app.use('/api',searchRouter)
+
+
 
 // Default route
 app.get('/get', (req, res) => {
   res.send('Fitness App API is running!');
 });
 
-// Export app wrapped with serverless-http (this is what Vercel uses to handle requests)
-module.exports.handler = serverless(app);
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
